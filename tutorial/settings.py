04d012bc00ab8a35469e7b49cea39ee1e4a14a70
Dalvinder Singh <dalvinder83@gmail.com>
2012-08-22 11:41:18 -0700

    db changes

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -11,7 +11,7 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgressql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'tutorial',                      # Or path to database file if using sqlite3.
         'USER': 'django',                      # Not used with sqlite3.
         'PASSWORD': 'vohfiofehohquieraequooboingaewoo',                  # Not used with sqlite3.
