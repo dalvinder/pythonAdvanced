6fbf1eb14a60ed082e05242e1c7d9c7ad7500722
Dalvinder Singh <dalvinder83@gmail.com>
2012-08-22 13:08:02 -0700

    Switching to postgres

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -13,9 +13,13 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'tutorial',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':'/tmp/tutorial.sqllite3',
+        #'USER':'',
+        #'PASSWORD':'',
         'USER': 'django',                      # Not used with sqlite3.
         'PASSWORD': 'vohfiofehohquieraequooboingaewoo',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'HOST': '127.0.0.1',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
