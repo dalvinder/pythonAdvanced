e00b439cb77d19e101ae64d75cfa05ba05e0841d
Dalvinder Singh <dalvinder83@gmail.com>
2012-08-23 13:39:44 -0700

    named views and url creation

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,8 @@
 # Create your views here.
 from django.shortcuts import render_to_response, render, get_object_or_404
-from django.http import HttpResponse, Http404
-from polls.models import Poll
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from polls.models import Poll, Choice
+from django.core.urlresolvers import reverse
 #from django.template import loader, Context
 
 
@@ -34,9 +35,22 @@ def detail(request, poll_id):
     return render(request, 'polls/detail.html',{'poll':p})
 
 def results(request, poll_id):
-    return HttpResponse('You are looking at the results of poll %s' % poll_id)
+    #return HttpResponse('You are looking at the results of poll %s' % poll_id)
+    p = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html',{'poll':p})
 
 
 def vote(request, poll_id):
-    return HttpResponse('You are voting for poll %s' % poll_id)
-
+    #return HttpResponse('You are voting for poll %s' % poll_id)
+    p = get_object_or_404(Poll,pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html',{
+                'poll':p,
+                'error_message':'You did not select a choice',
+            })
+    else:
+        selected_choice.votes+=1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('results',args=(p.id,)))    
