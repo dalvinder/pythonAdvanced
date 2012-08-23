b1b5b1d0ee95bc329c85f4d8e7654d90ebd24c76
Dalvinder Singh <dalvinder83@gmail.com>
2012-08-23 13:56:19 -0700

    Generic view

--- polls/views.py
+++ polls/views.py
@@ -5,41 +5,6 @@ from polls.models import Poll, Choice
 from django.core.urlresolvers import reverse
 #from django.template import loader, Context
 
-
-def index(request):
-    latest_poll_list = Poll.objects.all().order_by('-pub_date')
-# Technique 1
-#    t=loader.get_template('polls/index.html')
-#    c = Context({
-#                 'latest_poll_list':latest_poll_list
-#        })
-#    return HttpResponse(t.render(c))
-    
-#Technique 2 
-#    return render_to_response('polls/index.html', {
-#                'latest_poll_list':latest_poll_list
-#            })
-
-#Technique 3 : Passes the user to the template(index.html)    
-    return render(request, 'polls/index.html', {
-                'latest_poll_list':latest_poll_list
-            })
-
-def detail(request, poll_id):
-    #return HttpResponse('You are looking at poll %s' % poll_id)
-#    try:
-#        p = Poll.objects.get(pk=poll_id)
-#    except Poll.DoesNotExist:
-#        raise Http404
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/detail.html',{'poll':p})
-
-def results(request, poll_id):
-    #return HttpResponse('You are looking at the results of poll %s' % poll_id)
-    p = get_object_or_404(Poll, pk=poll_id)
-    return render(request, 'polls/results.html',{'poll':p})
-
-
 def vote(request, poll_id):
     #return HttpResponse('You are voting for poll %s' % poll_id)
     p = get_object_or_404(Poll,pk=poll_id)
--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import patterns, include, url
 
-from polls.views import index
-
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
+from django.views.generic.list import ListView
+from polls.models import Poll
+from django.views.generic.detail import DetailView
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -16,9 +17,26 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    #url(r'^polls/$', 'poll.views.index'),
-    url(r'^polls/$', index),
-    url(r'^polls/(?P<poll_id>\d+)/$','polls.views.detail',name="detail"),
-    url(r'^polls/(?P<poll_id>\d+)/results/$','polls.views.results', name="results"),
+
+    url(r'^polls/$',
+        ListView.as_view(
+            queryset=Poll.objects.order_by('-pub_date'),
+            context_object_name = 'latest_poll_list',
+            template_name='polls/index.html'
+        ),
+        name="polls_home"),
+    url(r'^polls/(?P<pk>\d+)/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/detail.html'
+        ),
+        name="detail"),
+    url(r'^polls/(?P<pk>\d+)/results/$',
+        DetailView.as_view(
+            model=Poll,
+            template_name='polls/results.html'
+        ),
+        name="results"),
+    
     url(r'^polls/(?P<poll_id>\d+)/vote/$','polls.views.vote',name="vote"),
 )
